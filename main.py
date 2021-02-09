48141b23d44771d83a87afc7b622200d0f897590
Ninnjah <45268461+Ninnjah@users.noreply.github.com>
2021-02-10 02:40:59 +0300

    Добавил проверку капчи

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime as dt
 import time
 import vk_api
@@ -7,17 +8,31 @@ TOKEN = '<token>'
 MESSAGE = 'Тестовое сообщение'
 IDS = []
 
+vk = vk_api.VkApi(token=TOKEN).get_api()
+
+
 def send(id_):
     vk_id = IDS[id_]
-    msg = MESSAGE
     
-    vk.messages.send(
-        user_id=vk_id,
-        message=msg,
-        random_id=0
-    )
-
-vk = vk_api.VkApi(token=TOKEN).get_api()
+    try:
+        vk.messages.send(
+            user_id=vk_id,
+            message=MESSAGE + str(id_+1),
+            random_id=0
+        )
+    except vk_api.exceptions.Captcha as captcha:
+        print('Ошибка!\nВылезла капча..')
+        
+        captcha.sid
+        with open('captcha.png', 'wb') as img:
+            img.write(captcha.get_image())
+        os.system('captcha.png')
+        
+        key = ''
+        while key == '':
+            key = input('Введите ответ: ')
+        
+        captcha.try_again(key)
 
 while True:
     start_time = input('Введите время начала рассылки в формате "ЧЧ:ММ": ')
@@ -38,12 +53,5 @@ while dt.timestamp(dt.now()) < start_time:
 print("Рассылка началась!       \n----------------\n")
 for i in range(len(IDS)):
     time.sleep(DELAY)
-    try:
-        send(i)
-        print(f'сообщение {i} отправлено..')
-    except vk_api.exceptions.Captcha as captcha:
-        print('Ошибка вымени')
-        captcha.sid # Получение sid
-        captcha.get_url() # Получить ссылку на изображение капчи
-        captcha.get_image() # Получить изображение капчи (jpg)
-        send(i)
+    send(i)
+    print(f'сообщение {i + 1} отправлено..')
